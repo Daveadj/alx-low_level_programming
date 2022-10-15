51e82ac62a22929c9b382aeba3b1f613b0d6724f
Daveadj <ovoadjerese@gmail.com>
2022-10-15 03:28:24 -0700

    combo3

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print combo
+ * Return: 0
+ */
+int main(void)
+{
+	int n1, n2, n3;
+
+	for (n1 = '0'; n1 <= '9'; n1++)
+	{
+		for (n2 = n1 + 1; n2 <= '9'; n2++)
+		{
+			for (n3 = n2 + 1; n3 <= '9'; n3++)
+			{
+				putchar(n1);
+				putchar(n2);
+				putchar(n3);
+				if (n1 == '7' && n2 == '8' && n3 == '9')
+				{
+					continue
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
